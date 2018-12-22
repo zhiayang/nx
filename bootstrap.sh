09ff6fd63e55c3b851a187f786d684cba68d3ea5
zhiayang <zhiayang@gmail.com>
2018-12-23 02:09:26 +0800

    create a disk image and install refind on it

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # replace with where you cloned this
 export PROJECT_DIR=/mnt/d/Projects/nx
@@ -16,20 +16,39 @@ export TARGET=x86_64-orionx
 export BINUTILS_VERSION="2.31.1"
 export GCC_VERSION="8.2.0"
 
+
+SKIP_SETUP_LIBC=false
+SKIP_SETUP_SYSROOT=false
+SKIP_SETUP_TOOLCHAIN=false
+SKIP_SETUP_DISKIMAGE=false
+
+# setup colour codes
+_BOLD=`tput bold`
+_NORMAL=`tput sgr0`
+_RED=`tput setaf 1`
+_BLUE=`tput setaf 4`
+_GREEN=`tput setaf 2`
+
+
 function main() {
+
+	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}bootstrapping development environment${_NORMAL}"
+	echo ""
+
+
 	if [ ! -f "$PROJECT_DIR/utils/patches/binutils-$BINUTILS_VERSION.patch" ]; then
-		echo "binutils-$BINUTILS_VERSION is not supported! see utils/patches/ for a list of supported versions"
-		exit
+		echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}binutils-$BINUTILS_VERSION is not supported!${_NORMAL} see utils/patches/ for a list of supported versions"
+		exit 1
 	fi
 
 	if [ ! -f "$PROJECT_DIR/utils/patches/gcc-$GCC_VERSION.patch" ]; then
-		echo "gcc-$GCC_VERSION is not supported! see utils/patches/ for a list of supported versions"
-		exit
+		echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}gcc-$GCC_VERSION is not supported!${_NORMAL} see utils/patches/ for a list of supported versions"
+		exit 1
 	fi
 
 	if [ ! $(command -v pv) ]; then
-		echo "'pv' is used to display progress, please install it!"
-		exit
+		echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}'pv' not found${_NORMAL}"
+		exit 1
 	fi
 
 
@@ -47,18 +66,13 @@ function main() {
 
 
 
-	# setup colour codes
-	_BOLD=`tput bold`
-	_NORMAL=`tput sgr0`
-	_RED=`tput setaf 1`
-	_BLUE=`tput setaf 4`
-	_GREEN=`tput setaf 2`
+	if [ $SKIP_SETUP_SYSROOT == false ]; then
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}setting up sysroot${_NORMAL}"
+		mkdir -p $SYSROOT/usr/include
+		mkdir -p $SYSROOT/usr/lib
 
-
-	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}setting up sysroot${_NORMAL}"
-	mkdir -p $SYSROOT/usr/include/
-	mkdir -p $SYSROOT/usr/lib
-
+		mkdir -p $SYSROOT/boot
+	fi
 
 	echo "${_BOLD}${_GREEN}=> ${_NORMAL}prefix:  ${_BOLD}$PREFIX${_NORMAL}"
 	echo "${_BOLD}${_GREEN}=> ${_NORMAL}sysroot: ${_BOLD}$SYSROOT${_NORMAL}"
@@ -66,50 +80,56 @@ function main() {
 
 	cp -r libs/libc/include/* $SYSROOT/usr/include/
 
-	if [ "$1" != "--skip-toolchain" ]; then
+	if [ $SKIP_SETUP_TOOLCHAIN == false ]; then
 		export SETUP_DIR=$PROJECT_DIR/toolchain-setup
 		build_toolchain $SETUP_DIR
 		echo ""
 	fi
 
+	if [ $SKIP_SETUP_LIBC == false ]; then
+		# we need to build our libc with the cross compiler.
+		export CC=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-gcc
+		export CXX=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-g++
+		export LD=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-gcc
+		export CPP=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-cpp
+		export AR=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-ar
+		export AS=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-as
 
-	# we need to build our libc with the cross compiler.
-	export CC=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-gcc
-	export CXX=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-g++
-	export LD=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-gcc
-	export CPP=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-cpp
-	export AR=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-ar
-	export AS=$PROJECT_DIR/build/toolchain/bin/x86_64-orionx-as
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}building libm${_NORMAL}"
+		make -C libs/libm install > /dev/null
 
-	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}building libm${_NORMAL}"
-	make -C libs/libm install > /dev/null
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}building libc${_NORMAL}"
+		make -C libs/libc > /dev/null
 
-	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}building libc${_NORMAL}"
-	make -C libs/libc > /dev/null
+		echo ""
 
-	echo ""
+		unset CC
+		unset CXX
+		unset LD
+		unset CPP
+		unset AR
+		unset AS
+	fi
 
-	unset CC
-	unset CXX
-	unset LD
-	unset CPP
-	unset AR
-	unset AS
-
-
-	if [ "$1" != "--skip-toolchain" ]; then
+	if [ $SKIP_SETUP_TOOLCHAIN == false ]; then
 		export SETUP_DIR=$PROJECT_DIR/toolchain-setup
 		export PATH="$PREFIX/bin:$PATH"
 
 		mkdir -p $SETUP_DIR
 		pushd $SETUP_DIR > /dev/null
-			build_libstdcpp || { echo "${_BOLD}${_RED}=> ${_NORMAL}${_BOLD}libstdc++ compilation failed!${_NORMAL}"; exit 1; }
+			build_libstdcpp || { echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}libstdc++ compilation failed!${_NORMAL}"; exit 1; }
 			echo ""
 		popd > /dev/null
 	fi
 
 
-	echo "${_BOLD}${_GREEN}=> ${_NORMAL}${_BOLD}done!${_NORMAL}"
+	if [ $SKIP_SETUP_DISKIMAGE == false ]; then
+		$PROJECT_DIR/utils/tools/create-diskimage.sh || { echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}disk image creation failed!${_NORMAL}"; exit 1; }
+		echo ""
+	fi
+
+
+	echo "${_BOLD}${_GREEN}=> ${_NORMAL}${_BOLD}environment is set up!${_NORMAL}"
 }
 
 
@@ -121,11 +141,11 @@ function build_toolchain() {
 
 	mkdir -p $1
 	pushd $1 > /dev/null
-		build_binutils || { echo "${_BOLD}${_RED}=> ${_NORMAL}${_BOLD}binutils compilation failed!${_NORMAL}"; exit 1; }
+		build_binutils || { echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}binutils compilation failed!${_NORMAL}"; exit 1; }
 		echo ""
 
 		export PATH="$PREFIX/bin:$PATH"
-		build_gcc || { echo "${_BOLD}${_RED}=> ${_NORMAL}${_BOLD}gcc compilation failed!${_NORMAL}"; exit 1; }
+		build_gcc || { echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}gcc compilation failed!${_NORMAL}"; exit 1; }
 	popd > /dev/null
 }
 
@@ -142,7 +162,8 @@ function build_binutils() {
 
 	if [ ! -d "binutils-$BINUTILS_VERSION" ]; then
 		tar xf binutils-$BINUTILS_VERSION.tar.gz --checkpoint=.250
-		echo "\n${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}patching binutils${_NORMAL}"
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}patching binutils${_NORMAL}"
+		echo ""
 		pushd binutils-$BINUTILS_VERSION > /dev/null
 			patch -p1 < $PROJECT_DIR/utils/patches/binutils-$BINUTILS_VERSION.patch
 		popd > /dev/null
@@ -182,7 +203,8 @@ function build_gcc() {
 
 	if [ ! -d "gcc-$GCC_VERSION" ]; then
 		tar xf gcc-$GCC_VERSION.tar.gz --checkpoint=.250
-		echo "\n${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}patching gcc${_NORMAL}"
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}patching gcc${_NORMAL}"
+		echo ""
 		pushd gcc-$GCC_VERSION > /dev/null
 			patch -p1 < $PROJECT_DIR/utils/patches/gcc-$GCC_VERSION.patch
 		popd > /dev/null
@@ -229,19 +251,20 @@ function build_libstdcpp() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+for i in "$@" ; do [[ $i == "--skip-libc" ]] && SKIP_SETUP_LIBC=true && break ; done
+for i in "$@" ; do [[ $i == "--skip-sysroot" ]] && SKIP_SETUP_SYSROOT=true && break ; done
+for i in "$@" ; do [[ $i == "--skip-toolchain" ]] && SKIP_SETUP_TOOLCHAIN=true && break ; done
+for i in "$@" ; do [[ $i == "--skip-diskimage" ]] && SKIP_SETUP_DISKIMAGE=true && break ; done
 
 main "$@"
+
+
+
+
+
+
+
+
 
 
 
---
+++ utils/tools/create-diskimage.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+# setup colour codes
+_BOLD=`tput bold`
+_NORMAL=`tput sgr0`
+_RED=`tput setaf 1`
+_BLUE=`tput setaf 4`
+_GREEN=`tput setaf 2`
+
+REFIND_VERSION=0.11.4
+
+# 100 mb.
+SECTOR_SIZE=512
+SECTOR_COUNT=204800
+
+ESP_PARTITION_START=2048
+ESP_PARTITION_END=102400
+
+ROOT_PARTITION_START=104448
+ROOT_PARTITION_END=204766
+
+ESP_PART_UUID="46B5A7DF-4FB2-4545-9C9E-AE54345868F5"
+ROOT_PART_UUID="871A56D0-2069-EE41-AC8E-0B67C148A150"
+
+OUTPUT_DISK=disk.img
+
+
+if [ $PROJECT_DIR == "" ]; then
+	echo "please set the project directory! (or invoke using './bootstrap --skip-toolchain --skip-libc --skip-sysroot' to only run this script"
+	exit 1
+fi
+
+# check for requisite tools
+if [ ! $(command -v fdisk) ]; then
+	echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}'fdisk' not found${_NORMAL} (fdisk)"
+	exit 1
+fi
+
+if [ ! $(command -v mkfs.vfat) ]; then
+	echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}'mkfs.vfat' not found${_NORMAL} (dosfstools)"
+	exit 1
+fi
+
+if [ ! $(command -v unzip) ]; then
+	echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}'unzip' not found${_NORMAL} (unzip)"
+	exit 1
+fi
+
+if [ ! $(command -v mcopy) ]; then
+	echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}'mcopy' not found${_NORMAL} (mtools)"
+	exit 1
+fi
+
+
+
+
+echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}creating disk image${_NORMAL}"
+
+mkdir -p $PROJECT_DIR/build
+pushd $PROJECT_DIR/build > /dev/null
+
+	if [ -e $OUTPUT_DISK ]; then rm $OUTPUT_DISK; fi
+
+	echo "${_BOLD}${_GREEN}=> ${_NORMAL}${_BOLD}image size: $(expr $SECTOR_COUNT \* $SECTOR_SIZE / 1024 / 1024) mb${_NORMAL}"
+	echo ""
+	dd if=/dev/zero of=$OUTPUT_DISK bs=$SECTOR_SIZE count=$SECTOR_COUNT status=none
+
+	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}downloading rEFInd${_NORMAL}"
+	if [ ! -d $PROJECT_DIR/utils/refind-bin-$REFIND_VERSION ]; then
+		wget -q --show-progress https://downloads.sourceforge.net/project/refind/$REFIND_VERSION/refind-bin-$REFIND_VERSION.zip
+		unzip -qq -d $PROJECT_DIR/utils refind-bin-$REFIND_VERSION.zip
+
+		rm refind-bin-$REFIND_VERSION.zip
+	fi
+
+	echo ""
+
+	# make temporary disk images for our esp image and our data image, which we will later `dd` into the disk.img at the appropriate offsets.
+
+	if [ -e esp-part.img ]; then rm esp-part.img; fi
+	if [ -e root-part.img ]; then rm root-part.img; fi
+
+	dd if=/dev/zero of=esp-part.img bs=$SECTOR_SIZE count=$(expr $ESP_PARTITION_END - $ESP_PARTITION_START + 1) status=none
+	dd if=/dev/zero of=root-part.img bs=$SECTOR_SIZE count=$(expr $ROOT_PARTITION_END - $ROOT_PARTITION_START + 1) status=none
+
+	if [ $(uname) == "Darwin" ]; then
+		echo "osx"
+	elif [ $(uname) == "Linux" ]; then
+
+		# make the filesystems -- this is os-specific so we drop back into the if-statement for a while
+		echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}creating GPT${_NORMAL}"
+
+		# a transcript:
+		# 1. g                    - make a new guid partition table
+		# 2. n                    - make a new partition
+		# 3. 1                    - partition numero uno
+		# 4. ESP_PARTITION_START  - partition start
+		# 5. ESP_PARTITION_END    - partition end
+		# 6. n                    - make a new partition
+		# 7. 2                    - partition numero dos
+		# 8. ROOT_PARTITION_START - partition start
+		# 9. ROOT_PARTITION_END   - partition end
+		# 10. t                   - change partition type
+		# 11. 1                   - select partition 1
+		# 12. 01                  - EFI system
+		# 13. t                   - change partition type
+		# 14. 2                   - select partition 2
+		# 15. 11                  - microsoft basic data (fat32)
+		# 16. x                   - expert mode
+		# 17. u                   - change partition uuid
+		# 18. 1                   - select partition 1
+		# 19. ESP_PART_UUID       - uuid
+		# 20. u                   - change partition uuid
+		# 21. 2                   - select partition 2
+		# 22. ROOT_PART_UUID      - uuid
+		# 23. r                   - return to non-expert menu
+		# 24. w                   - write changes and quit
+
+		echo -e "g\nn\n1\n$ESP_PARTITION_START\n$ESP_PARTITION_END\nn\n2\n$ROOT_PARTITION_START\n$ROOT_PARTITION_END\nt\n1\n01\nt\n2\n11\nx\nu\n1\n\
+$ESP_PART_UUID\nu\n2\n$ROOT_PART_UUID\nr\nw\n" | fdisk $OUTPUT_DISK > /dev/null
+
+		mkfs.vfat -n "EFI" -F 32 esp-part.img > /dev/null
+		mkfs.vfat -n "NX" -F 32 root-part.img > /dev/null
+
+	else
+		echo "${_BOLD}${_RED}!> ${_NORMAL}${_BOLD}$(uname) is not a supported build platform!${_NORMAL}"
+		exit 1
+	fi
+
+	# go back to os-agnostic things.
+
+	# copy refind to the ESP
+	echo "${_BOLD}${_BLUE}=> ${_NORMAL}${_BOLD}installing rEFInd to ESP${_NORMAL}"
+
+	mmd -i esp-part.img ::/EFI
+	mmd -i esp-part.img ::/EFI/BOOT
+	mmd -i esp-part.img ::/EFI/BOOT/refind
+
+	mcopy -sQ -i esp-part.img $PROJECT_DIR/utils/refind.conf ::/EFI/BOOT/refind/refind.conf
+	mcopy -sQ -i esp-part.img $PROJECT_DIR/utils/refind-bin-$REFIND_VERSION/refind/drivers_x64 ::/EFI/BOOT/refind/
+	mcopy -sQ -i esp-part.img $PROJECT_DIR/utils/refind-bin-$REFIND_VERSION/refind/tools_x64 ::/EFI/BOOT/refind/
+	mcopy -sQ -i esp-part.img $PROJECT_DIR/utils/refind-bin-$REFIND_VERSION/refind/refind_x64.efi ::/EFI/BOOT/bootx64.efi
+
+
+	# join the two partitions together.
+	dd if=esp-part.img of=disk.img bs=$SECTOR_SIZE count=$(expr $ESP_PARTITION_END - $ESP_PARTITION_START + 1) seek=$ESP_PARTITION_START conv=notrunc status=none
+	dd if=root-part.img of=disk.img bs=$SECTOR_SIZE count=$(expr $ROOT_PARTITION_END - $ROOT_PARTITION_START + 1) seek=$ROOT_PARTITION_START conv=notrunc status=none
+
+	# delete the temp files
+	rm esp-part.img root-part.img
+
+popd > /dev/null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
