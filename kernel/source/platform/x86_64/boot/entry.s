4766f538e1fcafcec353ab2afa24e18237368a40
zhiayang <zhiayang@gmail.com>
2019-02-02 23:05:39 +0800

    ensure 16-byte aligned stack for SSE stuff
    apparently qemu does not care... so we didn't know.

--- kernel/source/platform/x86_64/boot/entry.s
+++ kernel/source/platform/x86_64/boot/entry.s
@@ -28,6 +28,18 @@ kernel_entry:
 	movq $0x0, %rbp
 
 
+	// Setup SSE
+	mov %cr0, %rax
+	and $0xFFFB, %ax        // clear coprocessor emulation CR0.EM
+	or $0x02, %ax           // set coprocessor monitoring  CR0.MP
+	mov %rax, %cr0
+	mov %cr4, %rax
+	orq $0x10600, %rax      // set CR4.OSFXSR, CR4.OSXMMEXCPT and CR4.FSGSBASE at the same time
+	orq $0x600, %rax
+	mov %rax, %cr4
+
+
+
 	// clear the wp bit in cr0 so ring0 can read/write all pages (without needing PAGE_WRITE)
 	mov %cr0, %eax
 	andl $0xfffeffff, %eax
@@ -52,7 +64,21 @@ kernel_entry:
 kernel_premain:
 	// setup a null return address
 	xor %rbp, %rbp
-	push %rbp
+
+	// stack should be 16-byte aligned on entry into a function
+	// but, the function will push %rbp as part of its prologue
+	// so, we make sure the stack is *not* 16-byte aligned (ie it is 0x......8)
+
+	mov %rsp, %rax
+	and $0xfffffffffffffff0, %rax
+	cmp %rax, %rsp
+
+	jne skip_align
+
+	sub $8, %rsp
+
+skip_align:
+
 	jmp _ZN2nx4initEPNS_8BootInfoE
 
 
